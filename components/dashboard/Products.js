45cf216f0b8f4b16d36cf84eb8ee5f0254c44251
kristbjörg <kristbjorg_@hotmail.com>
2021-03-18 14:10:28 +0000

    conponents db

--- components/dashboard/Products.js
+++ components/dashboard/Products.js
@@ -1,14 +1,47 @@
-import styles from "../../styles/Dashboard.module.css";
-import { auth } from "../../pages/fire";
+import styles from "../../styles/Dashboard/Products.module.css";
+import { auth, db } from "../../pages/fire";
 import { UsersContext } from "../../pages/context";
 import React, { useContext } from "react";
+import Image from "next/image";
+import { RiDeleteBin6Line } from "react-icons/ri";
 
 export default function Products() {
   const { userData, users, currentUser } = useContext(UsersContext);
+
+  // const handleDelete = async() => {
+  //   console.log(db);
+
+  //   const userSnapshot =
+  // };
+
   return (
     <div className={styles.component_container}>
-      <h1> Products</h1>
+      <h1> Mínar vörur </h1>
+
+      {userData.products.map((product) => {
+        return (
+          <div className={styles.product}>
+            <img className={styles.product_img} src={product.productImg} />
+            <div className={styles.product_info}>
+              <h2> {product.productDescription}</h2>
+              <p> {product.productPrice} KR</p>
+              <p> ID: {product.productId}</p>
+            </div>
+            <button className={styles.product_button}>Breyta</button>
+
+            <a
+              onClick={() => {
+                handleDelete();
+              }}
+            >
+              {" "}
+              <RiDeleteBin6Line /> Eyða
+            </a>
+
+            <hr />
+          </div>
+        );
+      })}
     </div>
   );
-  // return <p> {userData.products[0].productDescription} </p>;
 }
