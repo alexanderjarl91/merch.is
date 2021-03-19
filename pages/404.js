a412316a7477229484bacc83a9cc60b6815ea9c0
Paula Nerenberg <paulita.glam@gmail.com>
2021-03-19 12:01:36 +0000

    404 page added

--- pages/404.js
+++ pages/404.js
@@ -1,12 +1,18 @@
 import React from "react";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
+import styles from "../styles/page404.module.css";
 
 export default function Custom404() {
   return (
     <>
       <Navbar />
-      <div>404 page</div>
+     <div className="content">
+      <div className={styles.error404}>404 </div>
+      <div className={styles.errorText}>
+        Úps síðan fannst ekki 
+      </div>
+      </div>
       <Footer />
     </>
   );
