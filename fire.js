b192e4a3cba502a5994250a40504e72bdf8db717
Alexander <alexanderjarl91@gmail.com>
2021-03-23 22:04:01 +0100

    env test

--- fire.js
+++ fire.js
@@ -14,10 +14,26 @@ const firebaseConfig = {
 
 const fire = firebase.apps.length
   ? firebase.app()
-  : firebase.initializeApp(firebaseConfig);
+  : firebase.initializeApp(firebaseConfigEnv);
 
 export const auth = firebase.auth();
 export const db = firebase.firestore();
 export const storage = firebase.storage();
 
 export default fire;
+
+//get env variables from netlify
+export async function getStaticProps() {
+  const firebaseConfigEnv = await someService({
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID,
+  });
+
+  return {
+    props: { firebaseConfigEnv },
+  };
+}
