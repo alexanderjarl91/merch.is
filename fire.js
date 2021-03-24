4d8e1520165fbdc84c19415c3377d84bca2c9624
Alexander <alexanderjarl91@gmail.com>
2021-03-24 13:26:40 +0100

    merge

--- fire.js
+++ fire.js
@@ -16,6 +16,11 @@ const fire = firebase.apps.length
   ? firebase.app()
   : firebase.initializeApp(firebaseConfig);
 
+export const auth = firebase.auth();
+export const db = firebase.firestore();
+export const storage = firebase.storage();
+export default fire;
+
 // export async function getServerSideProps() {
 //   const firebaseConfigEnv = {
 //     apiKey: process.env.FIREBASE_API_KEY,
@@ -34,25 +39,3 @@ const fire = firebase.apps.length
 // }
 
 // getServerSideProps();
-
-export const auth = firebase.auth();
-export const db = firebase.firestore();
-export const storage = firebase.storage();
-export default fire;
-
-// //get env variables from netlify
-// export async function getStaticProps() {
-//   const firebaseConfigEnv = {
-//     apiKey: process.env.FIREBASE_API_KEY,
-//     authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-//     projectId: process.env.FIREBASE_PROJECT_ID,
-//     storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-//     messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-//     appId: process.env.FIREBASE_APP_ID,
-//   };
-
-//   return {
-//     props: { firebaseConfigEnv },
-//   };
-// }
-//
