2e9a17ec22e442349550dd4cc7ce6483ae44a809
Paula Nerenberg <paulita.glam@gmail.com>
2021-03-25 19:48:59 +0000

    SEO added globally

--- pages/_app.js
+++ pages/_app.js
@@ -4,6 +4,11 @@ import { UsersProvider } from "../context";
 function MyApp({ Component, pageProps }) {
   return (
     <UsersProvider>
+        <Head>
+        <title>merch.</title>
+        <meta name="description" content="Merch has a lot of options to sell your product" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <Component {...pageProps} />
     </UsersProvider>
   );
